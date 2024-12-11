b4ce149962225072db3bdcb05571d89b1217a575
supergioo0 <ciaffajunior@gmail.com>
2024-12-11 22:45:05 +0200

    test2

--- app.py
+++ app.py
@@ -2,4 +2,9 @@
 import streamlit as st
 from main import StudyBuddy  # Import from study_buddy.py
 
-st.write("Hello world")
+# Initialize StudyBuddy (which internally initializes both TheoryAgent and CreativeAgent)
+study_buddy = StudyBuddy(project_id=st.secrets['PROJECT_ID'], location="global", engine_id=st.secrets['ENGINE_ID'], model_name=st.secrets['MODEL_NAME'])
+
+# Streamlit page configuration
+st.set_page_config(page_title="Study Buddy AI", page_icon=":sunglasses:", layout="wide")
+st.title("Study Buddy AI Chat")
