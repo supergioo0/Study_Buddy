4fbace95e60f297aa8272260adce0ee93ac9c54a
supergioo0 <ciaffajunior@gmail.com>
2024-12-11 22:56:34 +0200

    test4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from main import StudyBuddy
 import os
 
 # Initialize StudyBuddy (which internally initializes both TheoryAgent and CreativeAgent)
-study_buddy = StudyBuddy(project_id=os.getenv('PROJECT_ID'), location="global", engine_id=os.getenv('ENGINE_ID'), model_name='gemini-1.5-pro')
+study_buddy = StudyBuddy(project_id="united-impact-440612-m8", location="global", engine_id="study-buddy_1731147577608", model_name='gemini-1.5-pro')
 
 # Streamlit page configuration
 st.set_page_config(page_title="Study Buddy AI", page_icon=":sunglasses:", layout="wide")
